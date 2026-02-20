543d974ef910351af01e5a6e45661d4e71be27eb
kellen yu <kcy24@cornell.edu>
2026-02-19 19:11:53 -0500

    I THINK CRC16 Works

--- crc16.cpp
+++ crc16.cpp
@@ -5,10 +5,10 @@ uint16_t CRC16_USB(uint8_t *data, size_t len) {
     uin16_t crc = 0xFFFF;
     for (int i=0; i<len; i++) {
         //calculate dividend
-        uint8_t pos = (uint8_t)((crc ^ (data[i] << 8)) >> 8);
+        uint8_t pos = (uint8_t)((crc ^ (data[i])) & 0xFF);
         //use LUT to get remainder, shift out old MSB and XOR in remainder
-        crc = (uint16_t)((crc << 8) ^ CRC16_USB_RLUT[pos]);
+        crc = (uint16_t)((crc >> 8) ^ CRC16_USB_RLUT[pos]);
     }
-    return crc;
+    return crc ^ 0xFFFF;
 }
 
