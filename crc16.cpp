106e8ed74cb4720fff4f56921e1c49374819f128
kellen yu <kcy24@cornell.edu>
2026-02-19 19:06:28 -0500

    CRC not working ts a waste

---
+++ crc16.cpp
@@ -0,0 +1,14 @@
+#include "crc16.h"
+
+uint16_t CRC16_USB(uint8_t *data, size_t len) {
+    //initial crc value 0xFFFF
+    uin16_t crc = 0xFFFF;
+    for (int i=0; i<len; i++) {
+        //calculate dividend
+        uint8_t pos = (uint8_t)((crc ^ (data[i] << 8)) >> 8);
+        //use LUT to get remainder, shift out old MSB and XOR in remainder
+        crc = (uint16_t)((crc << 8) ^ CRC16_USB_RLUT[pos]);
+    }
+    return crc;
+}
+
